7a52e77795b36cd9f481d950c75fd173f02954ee
abaur <abaur@student.42.fr>
2023-02-11 22:18:59 +0100

    Ex00 Implementations

--- Matrix.hpp
+++ Matrix.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 20:36:26 by abaur             #+#    #+#             */
-/*   Updated: 2023/02/11 20:49:38 by abaur            ###   ########.fr       */
+/*   Updated: 2023/02/11 22:15:51 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,97 @@ namespace ft
 		typedef K	column_type[HEIGHT];
 
 		Matrix(void);
-		Matrix(array_type);
 		Matrix(const Matrix&);
+		Matrix(const array_type&);
+		Matrix&	operator=(const Matrix&);
+		Matrix&	operator=(const array_type&);
 
 		array_type	array;
 
 		column_type&	operator[](int);
 		const column_type& 	operator[](int) const;
 
-		Matrix	operator+=(const Matrix);
-		Matrix	operator-=(const Matrix);
-		Matrix	operator*=(K);
-
 		// Ex 00
-		Matrix	add(const Matrix);
-		Matrix	sub(const Matrix);
-		Matrix	scl(K);
+		Matrix&	operator+=(const Matrix&);
+		Matrix&	operator-=(const Matrix&);
+		Matrix&	operator*=(K);
+		Matrix&	add(const Matrix&);
+		Matrix&	sub(const Matrix&);
+		Matrix&	scl(K);
 	};
+}
+
+
+/******************************************************************************/
+/* # Implementations                                                          */
+/******************************************************************************/
+
+namespace ft 
+{
+	template <class K, int W, int H>
+	Matrix<K,W,H>::Matrix(void) 
+	: array({0}) 
+	{}
+	template <class K, int W, int H>
+	Matrix<K,W,H>::Matrix(const Matrix& other) 
+	: Matrix(other.array) 
+	{}
+	template <class K, int W, int H>
+	Matrix<K,W,H>::Matrix(const array_type& other) {
+		for (int x=0; x<W; x++)
+		for (int y=0; y<H; y++)
+			this->array[x][y] = other[x][y];
+	};
+
+	template <class K, int W, int H>
+	Matrix<K,W,H>&	Matrix<K,W,H>::operator=(const Matrix& other) {
+		new (this) Matrix(other.array);
+	};
+	template <class K, int W, int H>
+	Matrix<K,W,H>&	Matrix<K,W,H>::operator=(const array_type& other) {
+		new (this) Matrix(other.array);
+	};
+
+
+	template <class K, int W, int H>
+	typename Matrix<K,W,H>::column_type&	Matrix<K,W,H>::operator[](int x)
+	{ return this->array[x]; };
+
+	template <class K, int W, int H>
+	const typename Matrix<K,W,H>::column_type&	Matrix<K,W,H>::operator[](int x) const
+	{ return this->array[x]; };
+}
+
+
+/******************************************************************************/
+/* ## Exercice 00                                                             */
+/******************************************************************************/
+
+namespace ft
+{
+	template <class K, int W, int H> 
+	Matrix<K,W,H>&	Matrix<K,W,H>::operator+=(const Matrix& other) {
+		for (int x=0; x<W; x++)
+		for (int y=0; y<H; y++)
+			this->array[x][y] += other.array[x][y];
+		return *this;
+	};
+	template <class K, int W, int H> 
+	Matrix<K,W,H>&	Matrix<K,W,H>::operator-=(const Matrix& other) {
+		for (int x=0; x<W; x++)
+		for (int y=0; y<H; y++)
+			this->array[x][y] -= other.array[x][y];
+		return *this;
+	};
+	template <class K, int W, int H> 
+	Matrix<K,W,H>&	Matrix<K,W,H>::operator*=(K scalar) {
+		for (int x=0; x<W; x++)
+		for (int y=0; y<H; y++)
+			this->array[x][y] *= scalar;
+		return *this;
+	};
+
+	template <class K, int W, int H> Matrix<K,W,H>&	Matrix<K,W,H>::add(const Matrix& other) { return *this += other; };
+	template <class K, int W, int H> Matrix<K,W,H>&	Matrix<K,W,H>::sub(const Matrix& other) { return *this -= other; };
+	template <class K, int W, int H> Matrix<K,W,H>&	Matrix<K,W,H>::scl(K scalar) { return *this *= scalar; };
 }
--- Vector.hpp
+++ Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 19:24:46 by abaur             #+#    #+#             */
-/*   Updated: 2023/02/11 20:50:14 by abaur            ###   ########.fr       */
+/*   Updated: 2023/02/11 22:17:50 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ namespace ft
 		typedef K	array_type[SIZE];
 
 		Vector(void);
-		Vector(array_type);
 		Vector(const Vector&);
+		Vector(const array_type&);
+		Vector&	operator=(const Vector&);
+		Vector&	operator=(const array_type&);
 
 		array_type	array;
 
@@ -29,13 +31,78 @@ namespace ft
 		K&	operator[](int);
 		K 	operator[](int) const;
 
+		// Ex 00
 		Vector&	operator+=(const Vector&);
 		Vector&	operator-=(const Vector&);
 		Vector&	operator*=(K);
-
-		// Ex 00
 		Vector&	add(const Vector&);
 		Vector&	sub(const Vector&);
 		Vector&	scl(K);
 	};
+}
+
+
+/******************************************************************************/
+/* # Implementations                                                          */
+/******************************************************************************/
+
+namespace ft 
+{
+	template <class K, int S>
+	Vector<K,S>::Vector(void) 
+	: array({0}) 
+	{}
+	template <class K, int S>
+	Vector<K,S>::Vector(const Vector& other) 
+	: Vector(other.array) 
+	{}
+	template <class K, int S>
+	Vector<K,S>::Vector(const array_type& other) {
+		for (int i=0; i<S; i++)
+			this->array[i] = other[i];
+	};
+
+	template <class K, int S>
+	Vector<K,S>&	Vector<K,S>::operator=(const Vector& other) {
+		new (this) Vector(other.array);
+	};
+	template <class K, int S>
+	Vector<K,S>&	Vector<K,S>::operator=(const array_type& other) {
+		new (this) Vector(other.array);
+	};
+
+
+	template <class K, int S> K&	Vector<K,S>::operator[](int i)       { return this->array[i]; };
+	template <class K, int S> K 	Vector<K,S>::operator[](int i) const { return this->array[i]; };
+}
+
+
+/******************************************************************************/
+/* ## Exercice 00                                                             */
+/******************************************************************************/
+
+namespace ft
+{
+	template <class K, int S> 
+	Vector<K,S>&	Vector<K,S>::operator+=(const Vector& other) {
+		for (int i=0; i<S; i++)
+			this->array[i] += other.array[i];
+		return *this;
+	};
+	template <class K, int S> 
+	Vector<K,S>&	Vector<K,S>::operator-=(const Vector& other) {
+		for (int i=0; i<S; i++)
+			this->array[i] -= other.array[i];
+		return *this;
+	};
+	template <class K, int S> 
+	Vector<K,S>&	Vector<K,S>::operator*=(K scalar) {
+		for (int i=0; i<S; i++)
+			this->array[i] *= scalar;
+		return *this;
+	};
+
+	template <class K, int S> Vector<K,S>&	Vector<K,S>::add(const Vector& other) { return *this += other; };
+	template <class K, int S> Vector<K,S>&	Vector<K,S>::sub(const Vector& other) { return *this -= other; };
+	template <class K, int S> Vector<K,S>&	Vector<K,S>::scl(K scalar) { return *this *= scalar; };
 }
