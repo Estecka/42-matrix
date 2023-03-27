a146cff18deced4ab7f599512e8a5e384dd8364e
abaur <abaur@student.42.fr>
2023-03-27 16:22:21 +0200

    Ex15 Imported mxfrust from Cub3D

--- proj.cpp
+++ proj.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 16:24:51 by abaur             #+#    #+#             */
-/*   Updated: 2023/03/27 15:28:00 by abaur            ###   ########.fr       */
+/*   Updated: 2023/03/27 16:03:39 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,26 @@ namespace ft
 		float	height = width / ratio;
 		float	depth  = far - near;
 
-
+		// The matrix here appear as transposed
 		return Matrix<float,4,4>({
-			{ 2*near/width,             0,                0,                0 },
-			{            0, 2*near/height,                0,                0 },
-			{            0,             0, (near+far)/depth, 2*near*far/depth },
-			{            0,             0,               -1,                0 },
+			{ 2*near/width,             0,                0,  0 },
+			{            0, 2*near/height,                0,  0 },
+			{            0,             0, (near+far)/depth, -1 },
+			{            0,             0, 2*near*far/depth,  0 },
+		});
+	}
+
+	Matrix<float,4,4>	frustrum(float xMin, float xMax, float yMin, float yMax, float zMin, float zMax){
+		float	width  = xMax - xMin;
+		float	height = yMax - yMin;
+		float	depth  = zMax - zMin;
+
+		// The matrix here appear as transposed
+		return Matrix<float,4,4>({
+			{ 2*-zMax/width,                  0,  (xMin+xMax)/width,  0 },
+			{             0,     2*-zMax/height,                  0,  0 },
+			{             0, (yMin+yMax)/height, -(zMax+zMin)/depth, -1 },
+			{             0,                  0,  2*zMax*zMin/depth,  0 },
 		});
 	}
 }
--- proj.hpp
+++ proj.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 16:23:33 by abaur             #+#    #+#             */
-/*   Updated: 2023/03/23 16:25:02 by abaur            ###   ########.fr       */
+/*   Updated: 2023/03/27 15:50:01 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@
 namespace ft
 {
 	Matrix<float,4,4>	projection(float fov, float ratio, float near, float far);
+	Matrix<float,4,4>	frustrum(float xMin, float xMax, float yMin, float yMax, float zMin, float zMax);
 }
