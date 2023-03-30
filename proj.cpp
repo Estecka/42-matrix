049cadf0f10fac9033467bdcd60fef879163a674
abaur <abaur@student.42.fr>
2023-03-30 12:25:29 +0200

    Ex15 Fix signs in mxfrust

--- proj.cpp
+++ proj.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 16:24:51 by abaur             #+#    #+#             */
-/*   Updated: 2023/03/27 16:03:39 by abaur            ###   ########.fr       */
+/*   Updated: 2023/03/30 12:23:29 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@
 namespace ft
 {
 	Matrix<float,4,4>	projection(float fov, float ratio, float near, float far){
-		float	width  = 2 * std::tan(fov);
+		float	width  = 2 * std::tan(fov/2);
 		float	height = width / ratio;
 		float	depth  = far - near;
 
 		// The matrix here appear as transposed
 		return Matrix<float,4,4>({
-			{ 2*near/width,             0,                0,  0 },
-			{            0, 2*near/height,                0,  0 },
-			{            0,             0, (near+far)/depth, -1 },
-			{            0,             0, 2*near*far/depth,  0 },
+			{ 2*near/width,             0,                 0,  0 },
+			{            0, 2*near/height,                 0,  0 },
+			{            0,             0, -(near+far)/depth, -1 },
+			{            0,             0, -2*near*far/depth,  0 },
 		});
 	}
 
@@ -39,8 +39,8 @@ namespace ft
 		return Matrix<float,4,4>({
 			{ 2*-zMax/width,                  0,  (xMin+xMax)/width,  0 },
 			{             0,     2*-zMax/height,                  0,  0 },
-			{             0, (yMin+yMax)/height, -(zMax+zMin)/depth, -1 },
-			{             0,                  0,  2*zMax*zMin/depth,  0 },
+			{             0, (yMin+yMax)/height,  (zMax+zMin)/depth, -1 },
+			{             0,                  0, -2*zMax*zMin/depth,  0 },
 		});
 	}
 }
