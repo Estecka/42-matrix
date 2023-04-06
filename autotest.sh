655379b9b7be10da6d7eadacc031747491465fa8
abaur <abaur@student.42.fr>
2023-04-06 17:54:59 +0200

    Moved tests and drafts into their own folders.

--- autotest.sh
+++ autotest.sh
@@ -2,21 +2,21 @@
 
 make \
 && ./ft_matrix.out \
-&& ./ex00.sh \
-&& ./ex01.sh \
-&& ./ex02.sh \
-&& ./ex03.sh \
-&& ./ex04.sh \
-&& ./ex05.sh \
-&& ./ex06.sh \
-&& ./ex07.sh \
-&& ./ex08.sh \
-&& ./ex09.sh \
-&& ./ex10.sh \
-&& ./ex11.sh \
-&& ./ex12.sh \
-&& ./ex13.sh \
-&& ./ex14.sh \
+&& ./test/ex00.sh \
+&& ./test/ex01.sh \
+&& ./test/ex02.sh \
+&& ./test/ex03.sh \
+&& ./test/ex04.sh \
+&& ./test/ex05.sh \
+&& ./test/ex06.sh \
+&& ./test/ex07.sh \
+&& ./test/ex08.sh \
+&& ./test/ex09.sh \
+&& ./test/ex10.sh \
+&& ./test/ex11.sh \
+&& ./test/ex12.sh \
+&& ./test/ex13.sh \
+&& ./test/ex14.sh \
 && echo -e "\n\t\t# EX-15\n" \
 && ./proj.out 1 1 2 20 -t -ndc="-1,-1,0;1,1,1" \
 ;
